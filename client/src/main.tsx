a0fe70f9dfcd748458d1879efecfed3ff7449d0b
maxgeorg99 <maxi.georg.mg@gmail.com>
2025-10-03 12:26:49 +0200

    try to adjust authority url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { getSpacetimeConfig } from './config/spacetime';
 import App from './App';
 
 const oidcConfig = {
-  authority: 'https://spacetimeauth.staging.spacetimedb.com/oidc',
+  authority: 'https://auth.spacetimedb.com/oidc/auth',
   client_id: 'client_031CSnBZhPFgz5oj5Alo0a',
   redirect_uri: `${window.location.origin}/callback`,
   scope: 'openid profile email',
